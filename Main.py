754e6ea0a97a7b18ac501a792e5bc6ec85c7688c
codercarly <codercarly@gmail.com>
2023-02-20 14:34:19 -0500

    Functionize the socket server, add try/catch to client

--- Main.py
+++ Main.py
@@ -1,23 +1,56 @@
+import errno
 import json
 import socket
+import subprocess
+import time
 
 # Sample dictionary being sent to microservice
-dataDict = {
-    "Dining Out": 1,
-    "Groceries": 2,
-    "Fun Stuff": 3,
-    "Rent": 4,
-    "Utilities": 5
+catDict = {
+    "Cat Toys": 1,
+    "Cat Food": 2,
+    "Cat Clothes": 3,
+    "Cat Spa": 4,
+    "Cat Vet": 5
+}
+
+dogDict = {
+    "Dog Toys": 1,
+    "Dog Food": 2,
+    "Dog Clothes": 3,
+    "Dog Spa": 4,
+    "Dog Vet": 5
 }
 
 # Endcode the dictionary to json
-dataToSend = json.dumps(dataDict).encode("utf-8")
+catData = json.dumps(catDict).encode("utf-8")
+dogData = json.dumps(dogDict).encode("utf-8")
 
-# Send json over socket
-print("Creating socket connection...")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 1234))
-s.sendall(dataToSend)
-print("Sending data to microservice...")
 
-s.close() # Close the socket when done
+keepGoing = True
+
+while True:
+    try:
+        print("Creating socket connection...")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.connect(("127.0.0.1", 1234))
+
+        userInput = input("Dog or Cat? ")
+
+        if userInput == "Dog":
+            s.sendall(dogData)
+            print("Sending dog data to microservice...")
+            keepGoing = True
+        elif userInput == "Cat":
+            s.sendall(catData)
+            print("Sending cat data to microservice...")
+            keepGoing = True
+        else:
+            print("All done!")
+            keepGoing = False
+
+        time.sleep(5) # Pause before closing the connection
+        s.close() # Close the socket when done
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            pass
--- PieChart.py
+++ PieChart.py
@@ -3,12 +3,6 @@ import matplotlib.pyplot as plt
 import socket
 
 '''
-    Initialize lists for Pie Chart
-'''
-labels = []
-values = []
-
-'''
     CreatePieChart takes in labels and values from the client and returns a Pie Chart
     representation of those values
 '''
@@ -32,35 +26,47 @@ def CreatePieChart(labels, values):
 '''
     Create a Socket Server to connect with the Client
 '''
-# Create a byte array to decode the json data sent by client
-tempData = bytearray()
+def server():
+    labels = []
+    values = []
 
-# Create a socket connection
-print("Creating socket connection...")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("127.0.0.1", 1234))
-s.listen(5) # Listen for data
-print("Listening for data...")
-conn, addr = s.accept()
+    # Create a byte array to decode the json data sent by client
+    tempData = bytearray()
 
-# If data is received...
-while True:
-    dataReceived = conn.recv(4096)
-    print("Data recieved...")
+    # Create a socket connection
+    print("Creating socket connection...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("127.0.0.1", 1234))
+    s.listen(5) # Listen for data
+    print("Listening for data...")
+    conn, addr = s.accept()
 
-    tempData = tempData + dataReceived
-    data = json.loads(tempData.decode("utf-8")) # Decode the json data sent by client
+    # If data is received...
+    while True:
+        dataReceived = conn.recv(4096)
+        print("Data received...")
 
-    # Get key/value pairs from the data
-    for key in data.keys():
-        labels.append(key)
-    for value in data.values():
-        values.append(value)
+        tempData = tempData + dataReceived
+        data = json.loads(tempData.decode("utf-8")) # Decode the json data sent by client
 
-    # Create the pie chart and display it for the user
-    print("Creating pie chart...")
-    CreatePieChart(labels, values)
+        # Get key/value pairs from the data
+        for key in data.keys():
+            labels.append(key)
+        for value in data.values():
+            values.append(value)
 
-    break
+        print(labels)
+        print(values)
+        # Create the pie chart and display it for the user
+        print("Creating pie chart...")
+        CreatePieChart(labels, values)
 
-conn.close() # Close the connection
+        s.close()
+        print("Closing connection...")
+
+        # Restart the socket server
+        server()
+
+# Start up the socket server
+server()
